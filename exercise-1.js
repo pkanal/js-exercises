cf8d50d94dbf71cbafae65d2941a72d7f996c59b
Tahuana <tahuana@gmail.com>
2018-02-07 20:32:10 -0500

    Exercise 1 - solution

--- exercise-1.js
+++ exercise-1.js
@@ -12,7 +12,7 @@ const flavours = [
 ];
 
 const transformStrings = array => {
-  // your solution here
+  return array.reduce((acc, curr) => (curr.length <= 20 ) ? acc.concat(curr.split("").reverse().join("")) : acc,[])
 };
 
 console.log(transformStrings(flavours));
